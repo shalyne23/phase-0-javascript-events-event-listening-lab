40db43c7510f3b9edb3a91460336f6e9ca028a57
shalyne23 <shalynelangat@gmail.com>
2024-07-08 13:00:07 +0300

    event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+
+const button = document.getElementById('button');
+
+function addingEventListener(){
+    
+alert('I was clicked!');
+button.addEventListener('click', addingEventListener);
 }
+addingEventListener()
